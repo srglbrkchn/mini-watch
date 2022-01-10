0fc4bd36175a04aff1eda85fd1b9ee4d47bba65a
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-10 23:00:40 +0100

    Add AM/PM and date feature to the watch. Final commit.

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const hourHand = document.getElementById("hour_hand");
 const minuteHand = document.getElementById("minute_hand");
 const secondHand = document.getElementById("second_hand");
 
+const txtElement = document.getElementById("text");
+
+let showDate = true;
+
 // Animating watch movement
 function animate() {
-
-
-
   const date = new Date();
+
+  const day = date.getDate();
+  const ampm = date.getHours() >= 12 ? "PM" : "AM";
+
 
   // Get current hours minutes and seconds
   const hour = date.getHours() + date.getMinutes()/60;
   const minute = date.getMinutes() + date.getSeconds()/60;
   const second = date.getSeconds() + date.getMilliseconds()/1000;
+
+  txtElement.textContent = showDate? day : ampm;
 
   // Rotate watch hands based on current time
   hourHand.setAttribute("transform", `rotate(${(360/12) * hour})`);
@@ -24,3 +31,7 @@ function animate() {
 }
 
 requestAnimationFrame(animate);
+
+txtElement.addEventListener("click", () => {
+  showDate = !showDate;
+});
